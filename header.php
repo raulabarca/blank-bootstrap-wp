58a39644d5c1f7b752c822fe65b2d561898fd361
Raul Abarca <raulabarka@gmail.com>
2016-02-05 18:36:39 +0100

    Added menu and stylesheet

--- header.php
+++ header.php
@@ -19,13 +19,38 @@
   <!-- Theme CSS Goes Here -->
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.css" type="text/css" media="all" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-
+  <link rel="stylesheet" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 
   <!-- Theme JavaScript Goes Here -->
   <script src="http://code.jquery.com/jquery.js"></script>
   <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js" type="text/javascript"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/main.js" type="text/javascript"></script>
 </head>
 <body <?php body_class(); ?>>
+  <!-- Nav Menu -->
+  <div id="menu">
+	<div id="title-menu" class="hidden-xs">
+  		<?php if ( is_home() ) : ?>
+  			<h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
+  		<?php elseif (is_category()) :?>
+  			<h1><?php single_cat_title(); ?></h1>
+  		<?php  elseif ( is_tag() ) : ?>
+  			<h1><?php single_tag_title(); ?></h1>
+  		<?php else: ?>
+  			<?php if(have_posts()) : ?>
+  				<?php while(have_posts()) : the_post(); ?>
+        		 		<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Permalink to <?php the_title_attribute(); ?>"><?php the_title(); ?> </a></h1>
+  				<?php endwhile; ?>
+  			<?php endif; ?>
+  		<?php endif; ?>
+  	</div>
+<i id="icon-menu" class="fa fa-bars fa-3x"></i>
+  </div>
+
+  <div id="togglemenu" class="dropdown-men">
+  	<?php wp_nav_menu( array('menu' => 'New Menu', 'container' => 'nav' )); ?>
+  </div>
+  <!-- Nav Menu end -->
   <h1 id="header">
     <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
   </h1>
